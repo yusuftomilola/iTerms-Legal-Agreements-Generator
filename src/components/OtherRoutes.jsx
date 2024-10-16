82de53520dcde567f6bbca68b7a2f5661a5e8574
Falade Yusuf <faladeyusuf54@gmail.com>
2024-10-16 10:28:23 +0100

    Deleted a component not needed

--- src/components/OtherRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { auth } from "../config/firebase";
-
-const OtherRoutes = () => {
-
-
-    return auth.currentUser ? <Outlet /> : <Navigate to={"/"}
-}
